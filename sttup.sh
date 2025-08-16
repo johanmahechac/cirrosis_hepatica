a9412add697b9681c979613b58fb85c8d99b2b72
RafaEMG1 <rafaemg96@gmail.com>
2025-08-16 14:53:11 -0500

    Delete sttup.sh

--- sttup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Setup script for PCA NHANES Streamlit App
-
-echo "Installing required Python packages..."
-pip install -r requirements.txt
-
-echo "Launching Streamlit app..."
-streamlit run app_pca_nhanes_filtros.py
