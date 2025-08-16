c82944fa46edad9b111cc64b88b26ca97ce08368
RafaEMG1 <rafaemg96@gmail.com>
2025-08-16 15:01:10 -0500

    Update streamlit.py

--- streamlit.py
+++ streamlit.py
@@ -4,6 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sns
 
+
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import LabelEncoder
 from sklearn.preprocessing import OrdinalEncoder
